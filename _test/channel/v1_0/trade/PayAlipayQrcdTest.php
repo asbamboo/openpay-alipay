b5675fd2bdb72b60058a92b95f90db62eb60d0ae
Lichunyin <licy2013@aliyun.com>
2018-10-26 17:17:25 +0800

    1.0.0

---
+++ _test/channel/v1_0/trade/PayAlipayQrcdTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace asbamboo\openpayAlipay\_test\channel\v1_0\trade;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ *
+ * @author 李春寅 <licy2013@aliyun.com>
+ * @since 2018年10月26日
+ */
+class PayAlipayQrcdTest extends TestCase
+{
+    public function testExecute()
+    {
+
+    }
+}
